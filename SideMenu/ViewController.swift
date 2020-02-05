10af8d954d28c50ee186859fc911f31d0b0b5fb1
joel <joelcrawford30@gmail.com>
2020-02-05 09:37:46 +0300

    Initial Commit

---
+++ SideMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SideMenu
+//
+//  Created by JOEL CRAWFORD on 2/5/20.
+//  Copyright © 2020 JOEL CRAWFORD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
